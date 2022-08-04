19aa49c98ad10e12b30e0612e5fe0a3976790eda
Sophia Teutschler <sophia@sophiestication.com>
2022-08-04 16:59:37 -0700

    Initial Commit

---
+++ Cappella/CappellaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CappellaApp.swift
+//  Cappella
+//
+//  Created by Sophia Teutschler on 8/4/22.
+//
+
+import SwiftUI
+
+@main
+struct CappellaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
