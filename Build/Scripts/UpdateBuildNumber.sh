b4c3acffb62af4ae5bad0347c5b773d428e80059
Sophia Teutschler <sophia@sophiestication.com>
2024-09-18 17:41:50 +0200

    Update configuration files

---
+++ Build/Scripts/UpdateBuildNumber.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Change to the project's root directory
+cd "$SRCROOT"
+
+BUILD_NUMBER_XCCONFIG="$SRCROOT/Build/Configuration/BuildNumber.xcconfig"
+echo $BUILD_NUMBER_XCCONFIG
+
+mkdir -p "$(dirname "$BUILD_NUMBER_XCCONFIG")"
+
+BUILD_NUMBER=$(git rev-list --count HEAD)
+echo "GIT_BUILD_NUMBER = $BUILD_NUMBER" > "$BUILD_NUMBER_XCCONFIG"
+echo "GIT_BUILD_NUMBER = $BUILD_NUMBER"
