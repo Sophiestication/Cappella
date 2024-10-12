4bc8193cc864789ffae0f02a5536aa7229357287
Sophia Teutschler <sophia@sophiestication.com>
2024-10-12 15:30:51 +0200

    add updated shortcut recorder style

--- Cappella/Keyboard Shortcuts/ShortcutRecorderView.swift
+++ Cappella/Keyboard Shortcuts/ShortcutRecorderView.swift
@@ -7,32 +7,24 @@ import AppKit
 
 struct ShortcutRecorderView: View {
     @FocusState private var isFocused: Bool
+
+    @State private var isSelected: Bool = false
     @State private var isRecording: Bool = false
 
     @Binding var keyboardShortcut: GlobalKeyboardShortcut?
     @State private var currentModifiers: NSEvent.ModifierFlags? = nil
 
     @Environment(\.pixelLength) var pixelLength
+    @Environment(\.colorScheme) var colorScheme
 
     var body: some View {
-        HStack {
-            if isRecording {
-                if let currentModifiers {
-                    Text("\(currentModifiers.description)")
-                } else {
-                    Text("Type shortcut")
-                        .foregroundStyle(.secondary)
-                }
-            } else if let keyboardShortcut {
-                Text("\(keyboardShortcut)")
-            } else {
-                Text("Click to record shortcut")
-                    .foregroundStyle(.secondary)
-            }
+        Group {
+            content
         }
         .padding(.horizontal)
         .frame(minWidth: 180.0)
 
+        .overlay(placeholder, alignment: .center)
         .overlay(clearButton, alignment: .trailing)
 
         .padding(.vertical, 6.0)
@@ -41,13 +33,19 @@ struct ShortcutRecorderView: View {
 
         .focusable()
         .focused($isFocused)
-        .onChange(of: isFocused, initial: false) { _, isFocued  in
+        .onChange(of: isFocused, initial: true) { _, newValue  in
+            isSelected = newValue
+
             if isFocused == false {
-                isRecording = false
-                currentModifiers = nil
+                cancel()
             }
         }
         .focusEffectDisabled()
+
+        .contentShape(backgroundShape)
+        .onTapGesture {
+            record()
+        }
 
         .onKeyPress(.return, phases: .down, action: { keyPress in // start recording
             guard isRecording == false else { return .ignored }
@@ -57,27 +55,26 @@ struct ShortcutRecorderView: View {
             return .handled
         })
 
-        .contentShape(backgroundShape)
-        .onTapGesture {
-            record()
-        }
-
         .onKeyPress { keyPress in
             guard isRecording else { return .ignored }
 
-//            if keyPress.key == .escape &&
-//               keyPress.modifiers.isEmpty {
-//                isRecording = false
-//            } else {
+            if keyPress.key == .escape &&
+               keyPress.modifiers.isEmpty {
+                // will cancel…
+            } else if keyPress.key == .tab &&
+                      keyPress.modifiers.isEmpty {
+                cancel()
+
+                return .ignored
+            } else {
                 if let event = NSApp.currentEvent {
                     keyboardShortcut = GlobalKeyboardShortcut(with: event)
                 } else {
                     keyboardShortcut = nil
                 }
-//            }
+            }
 
-            currentModifiers = nil
-            isRecording = false
+            cancel()
 
             return .handled
         }
@@ -90,6 +87,34 @@ struct ShortcutRecorderView: View {
                 currentModifiers = NSEvent.ModifierFlags(new)
             }
         }
+    }
+
+    @ViewBuilder
+    private var content: some View {
+        if isRecording, let currentModifiers {
+            Text("\(currentModifiers.description)")
+        } else if let keyboardShortcut {
+            Text("\(keyboardShortcut)")
+        } else {
+            Text("\(CompatibilityKeyEquivalent.command)")
+                .opacity(0.0)
+        }
+    }
+
+    @ViewBuilder
+    private var placeholder: some View {
+        Text(placeholderString)
+            .foregroundStyle(.secondary)
+            .font(.callout)
+            .opacity(isPlaceholderVisible ? 1.0 : 0.0)
+    }
+
+    private var isPlaceholderVisible: Bool {
+        return currentModifiers == nil && keyboardShortcut == nil
+    }
+
+    private var placeholderString: String {
+        isRecording ? "Type shortcut" : "Click to record shortcut"
     }
 
     @ViewBuilder
@@ -112,68 +137,10 @@ struct ShortcutRecorderView: View {
     }
 
     @ViewBuilder
-    private func makeBackgroundView() -> some View {
-        ZStack {
-            backgroundShape
-                .shadow(color: Color.blue, radius: isFocused ? 2.0 : 0.0)
-                .opacity(isFocused ? 1.0 : 0.0)
-            backgroundShape
-                .fill(.background)
-
-            if isRecording && isFocused {
-                backgroundShape
-                    .fill(.selection)
-            } else if isFocused {
-                backgroundShape
-                    .stroke(.selection)
-            } else {
-                backgroundShape
-                    .stroke(.tertiary)
-            }
-        }
-        .animation(.smooth, value: isFocused)
-    }
-
-    @ViewBuilder
     private var background: some View {
-        ZStack {
-            backgroundShape
-                .fill(.tint.opacity(isFocused ? 1.0 : 0.0))
-                .blur(radius: 1.0)
-            backgroundShape
-                .fill(.tint.opacity(isRecording ? 1.0 : 0.0))
-                .blur(radius: 3.0)
-            backgroundShape
-                .fill(.foreground.opacity(1.0 / 7.0))
-                .offset(y: pixelLength)
-                .blur(radius: pixelLength)
-                .blendMode(.plusLighter)
-            backgroundShape
-                .fill(.background)
-            backgroundShape
-                .fill(.tint.opacity(isRecording ? 1.0 : 0.0))
-                .blendMode(.overlay)
-            backgroundShape
-                .fill(LinearGradient(
-                    gradient: backgroundGradient,
-                    startPoint: .top,
-                    endPoint: .bottom)
-                )
-            backgroundShape
-                .innerShadow(
-                    color: .black.opacity(1.0 / 3.0),
-                    radius: 1.0,
-                    x: 0,
-                    y: 1.0
-                )
-                .blendMode(.multiply)
-            backgroundShape
-                .inset(by: pixelLength)
-                .stroke(.black.opacity(1.0 / 6.0), lineWidth: pixelLength)
-        }
-        .animation(
-            .spring.repeatForever(autoreverses: true),
-            value: isRecording
+        ShortcutRecorderBackgroundView(
+            isFocused: $isSelected,
+            isRecording: $isRecording
         )
     }
 
@@ -195,10 +162,95 @@ struct ShortcutRecorderView: View {
         keyboardShortcut = nil
     }
 
-    private func clear() {
+    private func cancel() {
         currentModifiers = nil
         keyboardShortcut = nil
         isRecording = false
+    }
+
+    private func clear() {
+        cancel()
+    }
+}
+
+fileprivate struct ShortcutRecorderBackgroundView: View {
+    @Binding var isFocused: Bool
+    @Binding var isRecording: Bool
+
+    @Environment(\.pixelLength) var pixelLength
+    @Environment(\.colorScheme) var colorScheme
+
+    @State private var isPulsing: Bool = false
+
+    var body: some View {
+        ZStack {
+            backgroundShape
+                .fill(.tint.opacity(isFocused && colorScheme == .dark ? 1.0 : 0.0))
+                .blur(radius: 1.0)
+            backgroundShape
+                .fill(.tint.opacity(isPulsing ? 1.0 : 0.0))
+                .blur(radius: 3.0)
+            backgroundShape
+                .fill(.white.opacity(1.0 / 7.0))
+                .offset(y: pixelLength)
+                .blur(radius: pixelLength)
+                .blendMode(.plusLighter)
+            backgroundShape
+                .fill(.background)
+            backgroundShape
+                .fill(.tint.opacity(isPulsing ? 1.0 : 0.0))
+                .blendMode(.overlay)
+            backgroundShape
+                .fill(LinearGradient(
+                    gradient: backgroundGradient,
+                    startPoint: .top,
+                    endPoint: .bottom)
+                )
+            backgroundShape
+                .innerShadow(
+                    color: isFocused ?
+                        .accentColor.opacity(1.0 / 3.0) :
+                            .black.opacity(1.0 / 3.0),
+                    radius: 1.0,
+                    x: 0,
+                    y: 1.0
+                )
+                .blendMode(.multiply)
+            backgroundShape
+                .inset(by: pixelLength)
+                .stroke(.black.opacity(1.0 / 6.0), lineWidth: pixelLength)
+            backgroundShape
+                .stroke(lineWidth: 1.0)
+                .fill(.tint.opacity(isFocused ? 1.0 / 2.0 : 0.0))
+                .blendMode(.multiply)
+        }
+
+        .onChange(of: isRecording, initial: true) { _, newValue in
+            if newValue {
+                withAnimation(
+                    .spring.repeatForever(autoreverses: true)
+                ) {
+                    isPulsing = true
+                }
+            } else {
+                withAnimation(.snappy) {
+                    isPulsing = false
+                }
+            }
+        }
+    }
+
+    private var backgroundShape: Capsule {
+        Capsule(style: .continuous)
+    }
+
+    private var backgroundGradient: Gradient {
+        let colors = stride(from: 0.0, to: 0.2, by: 0.05).map { value -> Color in
+            let opacity = UnitCurve.easeInOut.value(at: value)
+            return Color.white.opacity(opacity)
+        }
+
+        return Gradient(colors: colors)
     }
 }
 
