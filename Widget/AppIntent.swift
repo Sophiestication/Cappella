1e11865bf8191a1a268a61edc7c9f78a8b7502a8
Sophia Teutschler <sophia@sophiestication.com>
2024-11-29 19:16:09 +0100

    add basic widget

---
+++ Widget/AppIntent.swift
@@ -0,0 +1,15 @@
+//
+// Copyright © 2006-2024 Sophiestication Software. All rights reserved.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource { "Configuration" }
+    static var description: IntentDescription { "This is an example widget." }
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
---
+++ Widget/CappellaWidgetBundle.swift
@@ -0,0 +1,13 @@
+//
+// Copyright © 2006-2024 Sophiestication Software. All rights reserved.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct CappellaWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        CappellaWidget()
+    }
+}
